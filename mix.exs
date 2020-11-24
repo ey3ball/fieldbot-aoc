4d5dd35c8890d821aacfe7bbca3efccae9f999f9
Florian Vallée <florian@vallee.xyz>
2020-11-24 23:36:46 +0100

    mix: configure and run mongodb client

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Aoc.MixProject do
   def application do
     [
       mod: {Aoc, []},
-      extra_applications: [:logger]
+      extra_applications: [:logger, :mongodb, :poolboy]
     ]
   end
 
@@ -25,7 +25,9 @@ defmodule Aoc.MixProject do
       {:exirc, "~> 2.0.0"},
       {:hackney, "~> 1.16.0"},
       {:jason, "~> 1.2"},
-      {:quantum, "~> 3.0"}
+      {:quantum, "~> 3.0"},
+      {:mongodb, " >= 0.0.0"},
+      {:poolboy, " >= 0.0.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
