2f144ddc4ff5e7f2e3df866858d3a3e8de505ee4
Florian Vallée <florian@vallee.xyz>
2020-11-30 00:18:20 +0100

    cron: push leaderboard updates directly into database
    Do not go through IrcBot process first

--- lib/aoc/ircbot/aoc.ex
+++ lib/aoc/ircbot/aoc.ex
@@ -25,11 +25,6 @@ defmodule Aoc.IrcBot.Aoc do
 
   def handle_cast(:heartbeat, state) do
     scrape_time = DateTime.to_iso8601(DateTime.utc_now())
-    leaderboard = Aoc.Rank.Client.leaderboard("2018")
-    Mongo.insert_one(
-      :mongo, "leaderboard",
-      Map.put(leaderboard, "scrape_time", scrape_time)
-    )
     ExIRC.Client.msg(
         state[:client], :privmsg, @channel,
       "Scraped 2018 leaderboard at " <> scrape_time
--- lib/aoc/scheduler.ex
+++ lib/aoc/scheduler.ex
@@ -6,6 +6,12 @@ defmodule Aoc.Scheduler do
   end
 
   def aocbot_heartbeat() do
+    scrape_time = DateTime.to_iso8601(DateTime.utc_now())
+    leaderboard = Aoc.Rank.Client.leaderboard("2018")
+    Mongo.insert_one(
+      :mongo, "leaderboard",
+      Map.put(leaderboard, "scrape_time", scrape_time)
+    )
     GenServer.cast(Process.whereis(:aocbot), :heartbeat)
   end
 end
