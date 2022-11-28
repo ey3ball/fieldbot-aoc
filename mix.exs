107c841841522ac452e1edb72244e57391fdd386
Florian Vallée (ey3ball) <florian@vallee.xyz>
2022-11-28 20:57:01 +0100

    wip: update deps, app goes up

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Aoc.MixProject do
     [
       app: :aoc,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -26,10 +26,11 @@ defmodule Aoc.MixProject do
       {:hackney, "~> 1.16.0"},
       {:jason, "~> 1.2"},
       {:quantum, "~> 3.0"},
-      {:mongodb, " >= 0.0.0"},
+      {:mongodb, github: "elixir-mongo/mongodb", branch: "otp/24"},
       {:poolboy, " >= 0.0.0"},
       {:floki, "~> 0.29.0"},
       {:tzdata, "~> 1.0.5"},
+      {:polyjuice_client, "~> 0.4.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
