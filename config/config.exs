96d93062992c45106bf08c89fa9c07ec159cc0f0
Florian Vallée (ey3ball) <florian@vallee.xyz>
2022-12-01 21:01:21 +0100

    config: schedule daily solution thread

--- config/config.exs
+++ config/config.exs
@@ -7,5 +7,6 @@ config :aoc, Aoc.Scheduler,
     {"0 4 * * *", &Aoc.Scheduler.aocbot_updates/0},
     {"0 1,12,18 * * *", &Aoc.Scheduler.aocbot_stats/0},
     {"1 5 * * *", &Aoc.Scheduler.aocbot_today/0},
+    {"0 8 * * *", &Aoc.Scheduler.aocbot_solutions/0},
     {"5,20,35,50 * * *", &Aoc.Scheduler.aocbot_heartbeat/0}
   ]
